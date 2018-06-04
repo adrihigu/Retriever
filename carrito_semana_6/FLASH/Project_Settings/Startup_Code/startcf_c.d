54570a34c4bf2da05e27468d9a1d062697319f06
adrihigu <higuereyadrian@gmail.com>
2018-06-04 16:39:33 -0400

    Add files via upload

---
+++ carrito_semana_6/FLASH/Project_Settings/Startup_Code/startcf_c.d
@@ -0,0 +1,6 @@
+Project_Settings/Startup_Code/startcf_c.obj: C:\Users\user.LC\carrito_semana_6\Project_Settings\Startup_Code\startcf.c \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Users\user.LC\carrito_semana_6\Project_Settings\Startup_Code\startcf.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_Runtime\Runtime_ColdFire\Source\RuntimeConfig.h 
