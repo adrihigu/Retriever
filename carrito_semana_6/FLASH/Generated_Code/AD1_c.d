1896a68dd97b37b25833212a377f9f6f6de0d069
adrihigu <higuereyadrian@gmail.com>
2018-06-04 14:19:13 -0400

    Add files via upload
    Perro con Sharp listo

---
+++ carrito_semana_6/FLASH/Generated_Code/AD1_c.d
@@ -0,0 +1,29 @@
+Generated_Code/AD1_c.obj: C:\Users\user.LC\carrito_semana_6\Generated_Code\AD1.c \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Users\user.LC\carrito_semana_6\Sources\Events.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\PE_Types.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\stdint.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\ansi_parms.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\ewlGlobals.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\coldfire\ansi_prefix.CF.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\os_enum.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\cstdint \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\limits_api.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
+	C:\Program\ Files\Freescale\CW\ MCU\ v10.6\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_misra_types.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\PE_Error.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\PE_Const.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\IO_Map.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\PE_Timer.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\TI1.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\Cpu.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\TI2.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\AD1.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\AS1.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\PWM1.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\PWM2.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\AS2.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\M1m.h \
+	C:\Users\user.LC\carrito_semana_6\Generated_Code\M2m.h 
