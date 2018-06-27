a0d3d6a1ac0ef61f3e73dcaeb06c197509cf78ef
Ruben Vasquez <ruvasqm@gmail.com>
2018-06-26 23:28:49 -0400

    ultima limpieza de cw (plz)

--- WSRetriever/carrito/FLASH/Project_Settings/Startup_Code/startcf_c.d
+++
@@ -1,6 +0,0 @@
-Project_Settings/Startup_Code/startcf_c.obj: C:\Users\Ruben\Documents\GitHub\Retriever\WSRetriever\carrito\Project_Settings\Startup_Code\startcf.c \
-	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
-	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
-	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
-	C:\Users\Ruben\Documents\GitHub\Retriever\WSRetriever\carrito\Project_Settings\Startup_Code\startcf.h \
-	C:\Freescale\CW\ MCU\ v10.7\MCU\ColdFire_Support\ewl\EWL_Runtime\Runtime_ColdFire\Source\RuntimeConfig.h 
